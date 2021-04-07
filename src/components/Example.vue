2f97dbcf105138d1ce5d9878435b4bb131246e43
Manuel Schulz <manuel.schulz@real-digital.de>
2021-04-07 12:30:39 +0200

    Initial commit

---
+++ src/components/Example.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <item-grid
+      min-width="100px"
+    >
+      <div
+        v-for="i in 12"
+        :key="`item-${i}`"
+        :class="$style.testItem"
+      >
+        <span :class="$style.itemTitle">Item {{ i }}</span>
+      </div>
+    </item-grid>
+
+    <div :class="$style.withSidebar">
+      <aside :class="$style.sidebar"> fixed width sidebar</aside>
+      <item-grid
+        min-width="100px"
+      >
+        <div
+          v-for="i in 12"
+          :key="`item-${i}`"
+          :class="$style.testItem"
+        >
+          <span :class="$style.itemTitle">Item {{ i }}</span>
+        </div>
+      </item-grid>
+    </div>
+  </div>
+</template>
+
+<script>
+import ItemGrid from './ItemGrid.vue';
+
+export default {
+  name: 'Example',
+  components: { ItemGrid },
+};
+</script>
+
+<style module lang="scss">
+.testItem {
+  background-color: silver;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 5em;
+}
+
+.itemTitle {
+  color: black;
+}
+
+.withSidebar {
+  display: grid;
+  grid-template-columns: 200px auto;
+  gap: 1em;
+  margin-top: 2em;
+}
+.sidebar {
+  border-right: 2px solid silver;
+}
+
+</style>
---
+++ src/components/ItemGrid.vue
@@ -0,0 +1,44 @@
+<template>
+  <div :class="$style.itemGrid" :style="gridStyle">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'item-grid',
+  props: {
+    minWidth: {
+      type: String,
+      default: '200px',
+    },
+    maxWidth: {
+      type: String,
+      default: '1fr',
+    },
+    gapHorizontal: {
+      type: String,
+      default: '1em',
+    },
+    gapVertical: {
+      type: String,
+      default: '1em',
+    },
+  },
+  computed: {
+    gridStyle() {
+      return {
+        color: 'red',
+        'grid-template-columns': `repeat(auto-fill, minmax(${this.minWidth}, ${this.maxWidth})`,
+        gap: `${this.gapHorizontal} ${this.gapVertical}`,
+      };
+    },
+  },
+};
+</script>
+
+<style module lang="scss">
+.itemGrid {
+  display: grid;
+}
+</style>
